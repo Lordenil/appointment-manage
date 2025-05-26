bd93949ffe4e4836635dd4f2c473851e51f687a3
dalvarez <dalvarez@tsgroup.com.co>
2025-05-26 09:59:17 -0500

    ADD: schedule module and this config by employee

--- src/main/java/com/manage/appointment/service/IUserService.java
+++ src/main/java/com/manage/appointment/service/IUserService.java
@@ -2,7 +2,6 @@ package com.manage.appointment.service;
 
 import com.manage.appointment.dto.UserDTO;
 import com.manage.appointment.entity.User;
-import org.apache.coyote.BadRequestException;
 
 public interface IUserService {
     public User registerUser(UserDTO user);
